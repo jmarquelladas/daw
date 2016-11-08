b0549b17ed0f2153a453988045f9c2a767c0faa6
Jose Miguel Arquelladas <jmaruiz@gmail.com>
2016-11-08 14:00:24 +0100

    Formulario login maquetado

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
 	<title>Entrada</title>
 	<meta charset="utf-8">
@@ -9,28 +9,36 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<div class="container">
-		<img src="./include/img/logo100.png" alt="Logo Empresa" class="img-responsive" height="25%" >
-		<h1>Gestión de permisos</h1>
-		<h2>Inicia sesión para acceder</h2>
-		<form>
-			<div class="form-group">
-				<label for="exampleInputEmail1">Usuario o Dirección de correo electrónico</label>
-				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Correo electrónico">
+	<div class="container-fluid">
+		<p></p>
+		<div class="row">
+			<div class="col-md-4"></div>
+			<div class="col-md-4">
+				<img src="./include/img/logo100.png" alt="Logo Empresa" class="img-responsive center-block" height="25%" >
+				<h1 class="text-center">Gestión de permisos</h1>
+				<h3>Inicia sesión para acceder</h3>
+				<form>
+					<div class="form-group">
+						<label for="exampleInputEmail1">Correo electrónico</label>
+						<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Correo electrónico">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Contraseña</label>
+						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Contraseña">
+					</div>
+					<div class="checkbox">
+						<label>
+							<input type="checkbox"> No cerrar sesión
+						</label>
+					</div>
+					<button type="submit" class="btn btn-default">Aceptar</button>
+					<a class="help-block" href="#">No recuerdo los datos de mi cuenta</a>
+				</form>
+				<a href="#">Crear cuenta</a>
 			</div>
-			<div class="form-group">
-				<label for="exampleInputPassword1">Contraseña</label>
-				<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Contraseña">
-			</div>
-			<div class="checkbox">
-				<label>
-					<input type="checkbox"> No cerrar sesión
-				</label>
-			</div>
-			<button type="submit" class="btn btn-default">Aceptar</button>
-			<a class="help-block" href="#">No recuerdo los datos de mi cuenta</a>
-		</form>
-		<a href="#">Crear cuenta</a>
+			<div class="col-md-4"></div>
+		</div>
+		<p></p>
 	</div>
 </body>
 </html>
