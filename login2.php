d902af5041368cd931276b199ce29716ced2c77d
José Miguel Arquelladas <jmaruiz@gmail.com>
2016-11-10 22:26:13 +0100

    Login version sin estilos

---
+++ login2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es-es">
+<head>
+	<title>Inicio de sesión</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="./include/css/principal.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+</head>
+<body>
+	<img src="./include/img/logo100.png" alt="Logo Empresa" height="25%" >
+	<h1>Gestión de permisos</h1>
+	<h3>Inicie sesión para acceder</h3>
+	<form>
+		<div>
+			<label for="email"></label>
+			<input type="email" placeholder="Correo electrónico">
+		</div>
+		<div>
+			<label for="contrase"></label>
+			<input type="password" placeholder="Contraseña">
+		</div>
+		<div>
+			<label>
+				<input type="checkbox"> No cerrar la sesión
+			</label>
+		</div>
+		<button type="submit">Aceptar</button>
+		<a href="#">No recuerdo los datos de mi cuenta</a>
+	</form>
+	<a href="#">Crear cuenta</a>
+</body>
+</html>
