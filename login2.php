b20473309e8e03d0cfd36ef2ae5ee10f3fabe5b6
José Miguel Arquelladas <jmaruiz@gmail.com>
2016-11-25 23:41:08 +0100

    Pie de página y comprobación de sesión

--- login2.php
+++ login2.php
@@ -3,8 +3,10 @@
 <head>
 	<title>Inicio de sesión</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="./include/css/principal.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Para adaptarlo a smartphones y Tablets -->
+	<!-- Hojas de estilo -->
+	<!-- Hoja nº1: Estilos de detalles complementarios -->
+	<link rel="stylesheet" href="./include/css/principal.css">
 	<!-- Para usar w3.css en la web es necesario realizar un enlace a "w3.css" -->
 	<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">	 <!-- Enlace al Framework del estilo w3.css -->
 </head>
@@ -14,23 +16,26 @@
 		<div class="w3-right-align w3-small">
 			<p><?php echo fecha(); ?></p>
 		</div>
-	</div>
-	<?php
-	if(isset($_REQUEST['aceptar'])) {
-		echo "Pulsado ACEPTAR";
-		if($_REQUEST['sesion'] = "1") {
-			echo "<p>Sesion no guardada</p>";
+		<?php
+		if(isset($_REQUEST['aceptar'])) {
+			echo "Pulsado ACEPTAR";
+			if($_REQUEST['sesion'] == "1") {
+				echo "<p>Sesion no guardada</p>";
+			} else {
+				echo "<p>Sesion guardada</p>";
+			}
 		}
-	}
-	?>
+		?>
+	</div>
 	<div class="w3-container">
 		<div class="w3-row">
 			<div class="w3-third w3-container"></div>
 			<div class="w3-third w3-container">
 				<center>
-					<img src="./include/img/logo100.png" alt="Logo Empresa" height="25%" class="w3-image">
-					<h1>Gestión de permisos</h1>
-					<h3>Inicie sesión para acceder</h3>
+					<img src="./include/img/logo100.png" alt="Logo Empresa" height="20%" class="w3-image">
+					<h2>Gestión de permisos</h2>
+					<h3>Iniciar sesión</h3>
+					<h4>Use su cuenta de usuario</h4>
 				</center>
 				<form class="w3-container">
 					<p>
@@ -58,6 +63,17 @@
 			<div class="w3-third w3-container"></div>
 		</div>
 	</div>
-	<div style="padding-top:60px"></div>
+	<div class="w3-container">
+		<footer>
+			<div class="w3-third w3-container"></div>
+			<div class="w3-third w3-container">
+				<center>
+					<a href="#">Condiciones de uso</a>
+					<a href="#">Privacidad y cookies</a>
+				</center>
+			</div>
+			<div class="w3-third w3-container"></div>
+		</footer>
+	</div>
 </body>
 </html>
